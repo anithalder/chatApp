1f8796f3263c1c5f7fd8f9dea9515b7df9b23764
Halka_Coding <87062016+anithalder@users.noreply.github.com>
2025-06-24 12:35:18 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
-import './App.css'
-
 function App() {
     return (
         <>
             <h1
                 className="text-4xl font-bold text-center"
             >
-                Welcome to Tailwind CSS with React!
+                <h1>Welcome to Tailwind CSS with React!</h1>
                 <button className="btn btn-accent">Accent</button>
             </h1>
         </>
